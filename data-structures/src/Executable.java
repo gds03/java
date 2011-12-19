6fb386ec4067704ff5037e4d5f82dc5ac3ce0d51
Goncalo Dias <goncaloRdias@hotmail.com>
2011-12-19 23:31:55 +0000

    Calculating CRUD times

--- data-structures/src/Executable.java
+++ data-structures/src/Executable.java
@@ -6,15 +6,59 @@ public class Executable {
 	 */
 	public static void main(String[] args) {
 		
-		Dictionary<Integer, String> dict = new Dictionary<>();
-		System.out.println("Starting test");
+		Dictionary<Integer, String> dict = new Dictionary<>(8196);
 		
-		for(int i = 0; i < 1000000; i++) {
+		//
+		// Insertion
+		//
+		
+		System.out.println("Starting test");
+		long start = System.currentTimeMillis();
+		
+		for(int i = 0; i < 5000000; i++) {
 			dict.insert(new Integer(i), "" + i);
 		}
 		
+		long end = System.currentTimeMillis();
+		
 		System.out.println("Ending test");
-
+		System.out.println("Insertion stage took: " + (end - start) + "ms");
+		
+		
+		//
+		// Searching
+		//
+		
+		System.out.println("Starting test");
+		start = System.currentTimeMillis();
+		
+		for(int i = 0; i < 5000000; i++) {
+			dict.search(new Integer(i));
+		}
+		
+		end = System.currentTimeMillis();
+		
+		System.out.println("Ending test");
+		System.out.println("Search stage took: " + (end - start) + "ms");
+		
+		
+		//
+		// Delete
+		// 
+		
+		System.out.println("Starting test");
+		start = System.currentTimeMillis();
+		
+		for(int i = 0; i < 5000000; i++) {
+			dict.remove(new Integer(i));
+		}
+		
+		end = System.currentTimeMillis();
+		
+		System.out.println("Ending test");
+		System.out.println("Delete stage took: " + (end - start) + "ms");
 	}
+	
+	
 
 }
